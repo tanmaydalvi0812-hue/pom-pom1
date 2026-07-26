36f1e2b52424b7ac0ccb5144b887d33f3f770c35
tanmaydalvi0812-hue <tanmaydalvi0812@gmail.com>
2026-07-26 08:20:32 +0530

    Update BotFather token and add admin user ID

--- config.py
+++ config.py
@@ -1,16 +1,7 @@
 import os
 
-# 🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴
-# 🔴  CHANGE 1: Put your BotFather bot token below         🔴
-# 🔴  Example: BOT_TOKEN = "7234567890:AAHd8f...xyz"       🔴
-# 🔴  Get it from @BotFather on Telegram                    🔴
-# 🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴
+# 🔴 CHANGE: Put your BotFather token
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8833030031:AAFtD-ilBv4aWJuYRfR3QdFVeeV4q6FRGJE")
 
-# 🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴
-# 🔴  CHANGE 2: Put YOUR Telegram user ID below          🔴
-# 🔴  ONLY this ID can access /admin panel               🔴
-# 🔴  Get it from @userinfobot on Telegram               🔴
-# 🔴  Example: ADMIN_IDS = [587463291]                   🔴
-# 🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴
+# 🔴 CHANGE: Put YOUR Telegram user ID
 ADMIN_IDS = [7217351605]
